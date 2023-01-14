faa818cf36036e0ffa595f5c5f118baffb7c1ab6
Matthew Lee <mlee2197@gmail.com>
2023-01-14 17:57:23 -0500

    add breakpoints

---
+++ src/utils/breakpoints.ts
@@ -0,0 +1,6 @@
+// mobile-first breakpoints
+export const largeMobileSize = "screen and (min-width: 640px)";
+export const tabletSize = "screen and (min-width: 768px)";
+export const landscapeTabletSize = "screen and (min-width: 1024px)";
+export const laptopSize = "screen and (min-width: 1280px)";
+export const desktopSize = "screen and (min-width: 1536px)";
